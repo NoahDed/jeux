57529406e31808ff3cb5aae8a011bcbbb024bca0
NoahDed <noah.dedina@outlook.fr>
2022-04-27 11:28:54 +0200

    Update Lancer de dés.py

--- Lancer de dés.py
+++ Lancer de dés.py
@@ -1,24 +1,10 @@
-from random import choice
- 
- 
-COUP = ("Pierre", "Feuille", "Ciseaux")
- 
- 
-while input("Jouez (y/n): ").lower() != "n":
- 
-    print("\n------------------------------------")
-    print("Le jeu du: Pierre - Feuille - Ciseaux")
-    print("------------------------------------\n")
- 
-    a = int(input("Choisissez un chiffre:\n0: Pierre\n1: Feuille\n2: Ciseaux\n-> "))
-    b = choice(range(3))
- 
-    print("\n{} VS {}".format(COUP[a], COUP[b]))
-    if a == b:
-        print("ÉGALITÉ\n")
-    elif (a>b and b+1==a) or (a<b and a+b==2):
-        print("VOUS GAGNEZ\n")
-    else:
-        print("VOUS PERDEZ\n")
-else:
-    print("Bye Bye")
+from random import randint
+
+repeat_rolling = True
+
+while repeat_rolling:
+
+    print("You rolled the following number using the Dice -",randint(1,6))
+    print("Do you wish to roll the dice again?")
+    
+    repeat_rolling = ("y" or "yes") in input().lower()
