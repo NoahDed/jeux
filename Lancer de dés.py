37494eb0fcaf186a6d06824e8d62428edb15de03
NoahDed <noah.dedina@outlook.fr>
2022-04-27 11:36:08 +0200

    Update Lancer de dés.py

--- Lancer de dés.py
+++ Lancer de dés.py
@@ -1,10 +1,11 @@
 from random import randint
+from tkinter import Y
 
 repeat_rolling = True
 
 while repeat_rolling:
 
-    print("You rolled the following number using the Dice -",randint(1,6))
-    print("Do you wish to roll the dice again?")
+    print("Vous avez obtenu le nombre suivant en utilisant le dé -",randint(1,6))
+    print("Voulez-vous relancer les dés ?")
     
     repeat_rolling = ("y" or "yes") in input().lower()
